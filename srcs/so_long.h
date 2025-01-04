509fbc09a9f3f3ae3c0200bb7e069222dd0d5a73
Ilyas Founas <ifounas@z4r6p5.42lyon.fr>
2025-01-04 19:00:32 +0100

    latest version

--- srcs/so_long.h
+++ srcs/so_long.h
@@ -27,19 +27,24 @@ typedef struct s_tab
 # include <fcntl.h>
 # include <stdio.h>
 
-//parsing.c
+// parsing.c
 size_t				count_wall(char *str);
 int					parsing_handling(int fd, char *path, t_size *window_size,
 						t_tab *matrice);
 
-//utils.c
+// utils.c
 t_tab				*ft_lstlast_sl(t_tab *lst);
 void				ft_lstadd_back_sl(t_tab **lst, t_tab *new);
 void				free_matrice(t_tab *matrice);
 
-//window_handling.c
-int					create_img(t_data img, int i, int	j, int color);
-void				create_map(t_data img, t_tab *matrice, t_size *window_size);
+// window_handling.c
+// int					create_img(t_data img, int i, int	j, int color);
+// void				create_map(t_data img, t_tab *matrice, t_size *window_size);
+
+int					create_img(int color, void *mlx, void *mlx_win, int axe_x,
+						int axe_y);
+void				create_map(t_tab *matrice, t_size *window_size, void *mlx,
+						void *mlx_win);
 void				window_handling(t_size *window_size, t_tab *matrice);
 
 #endif
--- srcs/window_handling.c
+++ srcs/window_handling.c
@@ -6,14 +6,78 @@
 /*   By: ifounas <ifounas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 12:10:07 by marvin            #+#    #+#             */
-/*   Updated: 2025/01/04 17:55:03 by ifounas          ###   ########.fr       */
+/*   Updated: 2025/01/04 18:59:46 by ifounas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minilibx-linux/mlx.h"
 #include "so_long.h"
 
-static void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+// first creat wall function
+// int	create_img(t_data img, int i, int	j, int color)
+// {
+// 	int	k;
+// 	int	q;
+
+// 	k = i;	
+// 	if (j == 0)
+// 		j++;
+// 	q = (j * 60) - 60;
+
+// 	while (i < k + 60)
+// 	{
+// 		while (q < j * 60)
+// 		{
+// 			my_mlx_pixel_put(&img, i, q, color);
+// 			q++;
+// 		}
+// 		q = (j * 60) - 60;
+// 		i++;
+// 	}
+// 	return (i);
+// }
+
+// void	create_map(t_data img, t_tab *matrice, t_size *window_size)
+// {
+// 	t_tab	*ptr_to_matrice;
+// 	int		i;
+// 	int		j;
+// 	int		axe_x;
+
+// 	i = 0;
+// 	j = 0;
+// 	axe_x = 0;
+// 	while (matrice)
+// 	{
+// 		ptr_to_matrice = matrice;
+// 		while (i < window_size->width)
+// 		{
+// 			if (ptr_to_matrice->tab != NULL)
+// 			{
+// 				if (ptr_to_matrice->tab[i] != 1 && ptr_to_matrice->tab[i] != 0)
+// 					ptr_to_matrice->tab[i] += 48;
+// 				if (ptr_to_matrice->tab[i] == 1)
+// 					axe_x = create_img(img, axe_x, j, 0xFFFFFFFF);
+// 				else if (ptr_to_matrice->tab[i] == 0)
+// 					axe_x = create_img(img, axe_x, j, 0x0000FFFF);
+// 				else if (ptr_to_matrice->tab[i] == 'C')
+// 					axe_x = create_img(img, axe_x, j, 0x0000F145);
+// 				else if (ptr_to_matrice->tab[i] == 'P')
+// 					axe_x = create_img(img, axe_x, j, 0x12456789);
+// 				else if (ptr_to_matrice->tab[i] == 'E')
+// 					axe_x = create_img(img, axe_x, j, 0xFFFFF145);
+// 			}
+// 			i++;
+// 		}
+// 		j++;
+// 		axe_x = 0;
+// 		i = 0;
+// 		matrice = matrice->next;
+// 	}
+// 	free_matrice(ptr_to_matrice);
+// }
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
@@ -21,44 +85,42 @@ static void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-// first creat wall function
-int	create_img(t_data img, int i, int	j, int color)
+int	create_img(int color, void *mlx, void *mlx_win, int axe_x, int axe_y)
 {
-	int	k;
-	int	q;
+	int	i;
+	int	j;
 	t_data img;
-
-	k = i;	
-	if (j == 0)
-		j++;
-	q = (j * 60) - 60;
-	// img.img = mlx_new_image(mlx, window_size->width * 60, window_size->height
-	// 		* 60);
-	// img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-	// 		&img.endian);
-	// mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	while (i < k + 60)
+	
+	i = 0;
+	j = 0;
+	color = 0;
+	printf("print\n");
+	img.img = mlx_new_image(mlx, 60, 60);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	while (i < 60)
 	{
-		while (q < j * 60)
+		while (j < 60)
 		{
-			my_mlx_pixel_put(&img, i, q, color);
-			q++;
+			my_mlx_pixel_put(&img, i, j, color);
+			j++;
 		}
-		q = (j * 60) - 60;
+		j = 0;
 		i++;
 	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, axe_x, (axe_y - 1)*60);
 	return (i);
 }
 
-void	create_map(t_data img, t_tab *matrice, t_size *window_size, void *mlx, void *mlx_win)
+void	create_map(t_tab *matrice, t_size *window_size, void *mlx, void *mlx_win)
 {
 	t_tab	*ptr_to_matrice;
 	int		i;
-	int		j;
+	int		axe_y;
 	int		axe_x;
 
 	i = 0;
-	j = 0;
+	axe_y = 0;
 	axe_x = 0;
 	while (matrice)
 	{
@@ -70,19 +132,19 @@ void	create_map(t_data img, t_tab *matrice, t_size *window_size, void *mlx, void
 				if (ptr_to_matrice->tab[i] != 1 && ptr_to_matrice->tab[i] != 0)
 					ptr_to_matrice->tab[i] += 48;
 				if (ptr_to_matrice->tab[i] == 1)
-					axe_x = create_img(img, axe_x, j, 0xFFFFFFFF);
+					axe_x += create_img(0xFFFFFFFF, mlx, mlx_win, axe_x, axe_y);
 				else if (ptr_to_matrice->tab[i] == 0)
-					axe_x = create_img(img, axe_x, j, 0x0000FFFF);
+					axe_x += create_img(0x0000FFFF, mlx, mlx_win, axe_x, axe_y);
 				else if (ptr_to_matrice->tab[i] == 'C')
-					axe_x = create_img(img, axe_x, j, 0x0000F145);
+					axe_x += create_img(0x0000F145, mlx, mlx_win, axe_x, axe_y);
 				else if (ptr_to_matrice->tab[i] == 'P')
-					axe_x = create_img(img, axe_x, j, 0x12456789);
+					axe_x += create_img(0x12456789, mlx, mlx_win, axe_x, axe_y);
 				else if (ptr_to_matrice->tab[i] == 'E')
-					axe_x = create_img(img, axe_x, j, 0xFFFFF145);
+					axe_x += create_img(0xFFFFF145, mlx, mlx_win, axe_x, axe_y);
 			}
 			i++;
 		}
-		j++;
+		axe_y++;
 		axe_x = 0;
 		i = 0;
 		matrice = matrice->next;
@@ -94,16 +156,16 @@ void	window_handling(t_size *window_size, t_tab *matrice)
 {
 	void	*mlx;
 	void	*mlx_win;
-	t_data	img;
+	// t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, window_size->width * 60, window_size->height
 			* 60, "SO_LONG");
-	img.img = mlx_new_image(mlx, window_size->width * 60, window_size->height
-			* 60);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-			&img.endian);
-	create_map(img, matrice, window_size, mlx, mlx_win);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	// img.img = mlx_new_image(mlx, window_size->width * 60, window_size->height
+	// 		* 60);
+	// img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+	// 		&img.endian);
+	create_map(matrice, window_size, mlx, mlx_win);
+	// mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
